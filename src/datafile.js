e7901de88b98315b38a3dcc2b4bea17da11702a7
Brie <carbon3dental@gmail.com>
2025-11-17 12:30:30 -0800

    Update files with react-router and modifications

---
+++ src/datafile.js
@@ -0,0 +1,23 @@
+export const dataFile = 
+    {
+       page: 1,
+       results: [
+        {
+            adult: false,
+            backdrop_path: "/path_to_backdrop.jpg",
+            genre_ids: [28, 12, 16],
+            id: 12345,  
+            original_language: "en",
+            original_title: "Sample Movie Title",
+            overview: "This is a sample overview of the movie. It provides a brief description of the plot and key elements.",
+            popularity: 150.123,    
+            poster_path: "/path_to_poster.jpg",
+            release_date: "2023-10-15",
+            title: "Sample Movie Title",
+            video: false,
+            vote_average: 7.8,
+            vote_count: 2560
+        }
+       ] 
+
+}
